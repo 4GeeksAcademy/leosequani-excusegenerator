6f695e1c7002498e53ccb84c7499ae35e5c43300
leosequani <156928045+leosequani@users.noreply.github.com>
2024-02-13 20:46:03 +0000

    finalsubmit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function makeTheExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car", "the bike"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
+  let strWho = who[Math.floor(Math.random() * 3)];
+  let strAction = action[Math.floor(Math.random() * 3)];
+  let strWhat = what[Math.floor(Math.random() * 3)];
+  let strWhen = when[Math.floor(Math.random() * 3)];
+  let excuse = `${strWho} ${strAction} ${strWhat} ${strWhen}`;
+  document.getElementById("excuse").innerHTML = excuse;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  makeTheExcuse();
 };
